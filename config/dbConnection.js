52be7449aeedf03b480223a498fe3080213e5f5b
Asendra Chauhan <asendrachauhan176@gmail.com>
2025-07-14 22:18:25 +0530

    updated dbConnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,29 @@
-// config/db.js
 import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config({
-  path: "./config/.env",
+  path: './config/.env',
 });
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 export default pool;
